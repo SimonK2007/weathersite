62f7955f6e1049d82e1bc2fbf6633c6175323953
SimonK2007 <simon.koppelstaetter@gmail.com>
2025-05-25 17:56:56 +0200

    next try

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: '/weathersite/',
 	plugins: [tailwindcss(), sveltekit()]
 });
